2749edd5e8c8018f30536222c70c77b5f5a73ee4
JeongWon-CHO <ahin0312@naver.com>
2025-03-12 21:05:12 +0900

    feat: 버튼 기능 연결

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -1,8 +1,39 @@
-import DeleteButton from 'src/assets/delete.svg';
-import NoSee from 'src/assets/no-see.svg';
+import { SetStateAction, useState } from 'react';
+import DeleteIcon from 'src/assets/delete.svg';
+import BlindIcon from 'src/assets/blind.svg';
+import EyeIcon from 'src/assets/eye.svg';
 import styles from './LoginPage.module.scss';
 
 export default function LoginPage() {
+  const [id, setId] = useState('');
+  const [pw, setPw] = useState('');
+  const [showPw, setShowPw] = useState<boolean>(false);
+
+  const currentId = (e: { target: { value: SetStateAction<string>; }; }) => {
+    setId(e.target.value);
+  };
+
+  const currentPw = (e: { target: { value: SetStateAction<string>; }; }) => {
+    setPw(e.target.value);
+  };
+
+  const deleteIdHandler = () => {
+    setId('');
+  };
+
+  const deletePwHandler = () => {
+    setPw('');
+  };
+
+  const onSubmit = (data: any) => {
+    console.log(data)
+    return;
+  };
+
+  const toggleShowPw = () => {
+    setShowPw((prev) => !prev);
+  };
+
   return (
     <div className={styles.container}>
       <div className={styles.container__input}>
@@ -10,23 +41,46 @@ export default function LoginPage() {
           <input
             placeholder='아이디를 입력하세요.'
             className={styles['container__input--text']}
+            onChange={currentId}
+            value={id}
           ></input>
-          <DeleteButton />
+          <button
+            onClick={deleteIdHandler}
+            className={styles['container__input--icon']}
+          >
+            <DeleteIcon />
+          </button>
         </div>
 
         <div className={styles['container__input--field-pw']}>
           <input
+            type={showPw ? "text" : "password"}
             placeholder='비밀번호를 입력하세요.'
-            className={styles['container__input--text']} 
+            className={styles['container__input--text']}
+            onChange={currentPw} 
+            value={pw}
           ></input>
           <div className={styles['container__input--icons']}>
-            <NoSee />
-            <DeleteButton />
+          <button
+              onClick={toggleShowPw}
+              className={styles['container__input--icon']}
+            >
+              {showPw ? <EyeIcon /> : <BlindIcon />}
+            </button>
+            <button
+              onClick={deletePwHandler}
+              className={styles['container__input--icon']}
+            >
+              <DeleteIcon />
+          </button>
           </div>
         </div>
       </div>
 
-      <button className={styles.container__submit}>
+      <button
+        className={styles.container__submit}
+        onClick={onSubmit}
+      >
         로그인
       </button>
 
