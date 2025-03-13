1e5c0d36408a17eb2cb386751845263b6f950dcd
Gwak-Seungju <tmwn1015@naver.com>
2025-03-13 17:14:57 +0900

    feat: 로고 클릭 시 메인 페이지로 이동

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,14 @@
 import WinectionLogo from '/src/assets/winection.svg';
 import styles from './Header.module.scss';
+import { useNavigate } from 'react-router-dom';
 
 export default function Header() {
+  const navigate = useNavigate();
   return (
     <div className={styles.header}>
-      <WinectionLogo />
+      <div className={styles.header__logo} onClick={() => navigate('/')}>
+        <WinectionLogo />
+      </div>
       <div className={styles.header__menu}>
         <button className={styles['header__menu--button']}>소개</button>
         <button className={styles['header__menu--button']}>개발자</button>
