ee0158785b78069f181abc4e73f37796091e5700
Gwak-Seungju <tmwn1015@naver.com>
2025-03-17 21:31:40 +0900

    fix: 로그인 페이지 style 추가

--- src/pages/LoginPage/index.tsx
+++ src/pages/LoginPage/index.tsx
@@ -2,12 +2,15 @@ import { SetStateAction, useState } from 'react';
 import DeleteIcon from 'src/assets/delete.svg';
 import BlindIcon from 'src/assets/blind.svg';
 import EyeIcon from 'src/assets/eye.svg';
+import { useNavigate } from 'react-router-dom';
 import styles from './LoginPage.module.scss';
 
 export default function LoginPage() {
+  const navigate = useNavigate();
   const [id, setId] = useState('');
   const [pw, setPw] = useState('');
   const [showPw, setShowPw] = useState<boolean>(false);
+  const [isError, setIsError] = useState(false);
 
   const currentId = (e: { target: { value: SetStateAction<string>; }; }) => {
     setId(e.target.value);
@@ -25,8 +28,21 @@ export default function LoginPage() {
     setPw('');
   };
 
-  const onSubmit = (data: any) => {
-    console.log(data)
+  const onSubmit = () => {
+    if (!id || !pw) {
+      setIsError(true);
+      return;
+    }
+    
+    // 로그인 api 나오면 없앨 예정
+    if (id !== 'tmdwn1234' || pw !== '!tmdwn1234') {
+      setIsError(true);
+      return;
+    }
+    sessionStorage.setItem('userInfo', JSON.stringify({ nickname: '아마승주', userClassification: '농인' }));
+
+    setIsError(false);
+    navigate('/');
     return;
   };
 
@@ -43,10 +59,11 @@ export default function LoginPage() {
             className={styles['container__input--text']}
             onChange={currentId}
             value={id}
-          ></input>
+          />
           <button
             onClick={deleteIdHandler}
             className={styles['container__input--icon']}
+            tabIndex={-1}
           >
             <DeleteIcon />
           </button>
@@ -59,39 +76,42 @@ export default function LoginPage() {
             className={styles['container__input--text']}
             onChange={currentPw} 
             value={pw}
-          ></input>
+          />
           <div className={styles['container__input--icons']}>
-          <button
+            <button
               onClick={toggleShowPw}
               className={styles['container__input--icon']}
+              tabIndex={-1}
             >
               {showPw ? <EyeIcon /> : <BlindIcon />}
             </button>
             <button
               onClick={deletePwHandler}
               className={styles['container__input--icon']}
+              tabIndex={-1}
             >
               <DeleteIcon />
-          </button>
+            </button>
           </div>
         </div>
       </div>
+      <div>
+        {isError && (
+          <div className={styles.error}>아이디 또는 비밀번호를 다시 확인해 주세요.</div>
+        )}
+        <button
+          className={styles.container__submit}
+          onClick={onSubmit}
+        >
+          로그인
+        </button>
+      </div>
 
-      <button
-        className={styles.container__submit}
-        onClick={onSubmit}
-      >
-        로그인
-      </button>
-
-      <ul className={styles['find-wrap']}>
-        <li>
-          <a className={styles['find-wrap__text-pw']}>비밀번호 찾기</a>
-        </li>
-        <li>
-          <a className={styles['find-wrap__text-join']}>회원가입</a>
-        </li>
-      </ul>
+      <div className={styles['find-wrap']}>
+        <button className={styles['find-wrap__button']} onClick={() => navigate('/find-pw')}>비밀번호 찾기</button>
+        <div>|</div>
+        <button className={styles['find-wrap__button']} onClick={() => navigate('/auth/signup')}>회원가입</button>
+      </div>
     </div>
   )
 }
