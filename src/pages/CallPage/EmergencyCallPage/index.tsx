a54f26aa81a5be413d767e83c6243b9835975096
Gwak-Seungju <tmwn1015@naver.com>
2025-03-29 17:25:23 +0900

    feat: 코드 입력 조건 추가

---
+++ src/pages/CallPage/EmergencyCallPage/index.tsx
@@ -0,0 +1,5 @@
+export default function EmergencyCallPage() {
+  return (
+    <div>긴급통화입니다.</div>
+  )
+}
---
+++ src/pages/CallPage/GeneralCallPage/index.tsx
@@ -0,0 +1,8 @@
+export default function GeneralCallPage() {
+
+  return (
+    <div>
+      통화페이지입니다.
+    </div>
+  )
+}
