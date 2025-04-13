9ddcfa1a191e5cecca47befcb6c50195806123b5
Gwak-Seungju <tmwn1015@naver.com>
2025-04-13 17:43:15 +0900

    feat: 응급통화 페이지 초기 단계

--- src/pages/CallPage/EmergencyCallPage/index.tsx
+++ src/pages/CallPage/EmergencyCallPage/index.tsx
@@ -1,5 +1,28 @@
+// import { useState } from "react";
+import styles from './EmergencyCallPage.module.scss';
+
+// 응급기관의 경우
+// 1. 농인이 아직 안 들어온 경우
+// 2. 농인이 들어온 경우
+
+// 농인의 경우
+// 1. 응급기관이 접수를 아직 안 한 경우
+// 2. 응급기관이 접수를 한 경우
+
+
+// 농인과 응급기관만 사용하는 페이지
 export default function EmergencyCallPage() {
+  const userInfo = JSON.parse(sessionStorage.getItem('userInfo')!);
+  
+  // const [peerStatus, setPeerStatus] = useState(false);
+  
+
   return (
-    <div>긴급통화입니다.</div>
+    <div className={styles.container}>
+      {userInfo === ''}
+      <div>
+        
+      </div>
+    </div>
   )
 }
