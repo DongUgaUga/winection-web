bedf7c63033eae5dbd3870eb7b82abd9b9675c0a
곽승주 <tmwn1015@naver.com>
2025-04-19 18:01:56 +0900

    chore: react-pwa 추가

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import * as path from "path";
 // import fs from "fs";
-
+import { VitePWA } from "vite-plugin-pwa";
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import svgr from 'vite-plugin-svgr';
@@ -8,7 +8,35 @@ import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), svgr({ include: '**/*.svg' }), tsconfigPaths()],
+  plugins: [
+    react(),
+    svgr({ include: '**/*.svg' }),
+    tsconfigPaths(),
+    VitePWA({
+      registerType: 'autoUpdate',
+      includeAssets: ['winection.ico', 'winection.png'],
+      manifest: {
+        name: 'Winection',
+        short_name: 'Winection',
+        start_url: '/',
+        display: 'standalone',
+        background_color: '#ffffff',
+        theme_color: '#ffffff',
+        icons: [
+          {
+            src: 'icons/winection_192.png',
+            sizes: '192x192',
+            type: 'image/png',
+          },
+          {
+            src: 'icons/winection_512.png',
+            sizes: '512x512',
+            type: 'image/png',
+          },
+        ],
+      },
+    }),
+  ],
   server: {
     /*
     https: {
