6cea6228a007dbe47961e7c645e23890cd0c45b5
곽승주 <tmwn1015@naver.com>
2025-04-20 20:48:19 +0900

    style: 응급기관 전화 전 페이지 모바일뷰 추가

--- src/pages/CallWaitPage/index.tsx
+++ src/pages/CallWaitPage/index.tsx
@@ -2,16 +2,20 @@ import { useParams } from 'react-router-dom';
 import GeneralCallWait from './GeneralCallWait';
 import EmergencyCallWait from './EmergencyCallWait';
 import styles from './CallWaitPage.module.scss';
+import useBreakpoint from '../../utils/hooks/useBreakPoint';
 
 export default function CallWaitPage() {
   const param = useParams();
+  const breakPoint = useBreakpoint();
   
   return (
     <div className={styles.container}>
-      <div className={styles.motto}>
-        <div>오늘도 우리는,</div>
-        <div><span className={styles.emphasize}>소통</span><span className={styles.gradient}>을 위해 달려나갑니다.</span></div>
-      </div>
+      {breakPoint !== 'mobile' && (
+        <div className={styles.motto}>
+          <div>오늘도 우리는,</div>
+          <div><span className={styles.emphasize}>소통</span><span className={styles.gradient}>을 위해 달려나갑니다.</span></div>
+        </div>
+      )}
       <div className={styles['before-call']}>
         {param.calltype === 'general-call'
           ? <GeneralCallWait />
