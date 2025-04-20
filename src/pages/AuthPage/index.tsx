b288dfc434cfa8f3fa105737da08ef00d40f6943
곽승주 <tmwn1015@naver.com>
2025-04-20 17:09:16 +0900

    feat: 모바일 auth페이지 헤더, 푸터 추가

--- src/pages/AuthPage/index.tsx
+++ src/pages/AuthPage/index.tsx
@@ -1,16 +1,28 @@
 import { Outlet, useNavigate } from 'react-router-dom';
 import WinectionIcon from 'src/assets/winection.svg';
 import styles from './AuthPage.module.scss';
+import useBreakpoint from '../../utils/hooks/useBreakPoint';
+import Header from '../../components/Header';
+import Footer from '../../components/Footer';
 
 export default function AuthPage() {
   const navigate = useNavigate();
+  const breakPoint = useBreakpoint();
 
   return (
-    <div className={styles.container}>
-      <div className={styles['container--logo']} onClick={() => navigate('/')}>
-        <WinectionIcon />
+    <>
+      {breakPoint === 'mobile' && (
+        <Header />
+      )}
+      <div className={styles.container}>
+       <div className={styles['container--logo']} onClick={() => navigate('/')}>
+         <WinectionIcon />
+       </div>
+       <Outlet />
       </div>
-      <Outlet />
-    </div>
+      {breakPoint === 'mobile' && (
+        <Footer />
+      )}
+    </>   
   )
 }
