41125319de3c6a7a69b4dec1ed4a1fd0004948ec
곽승주 <tmwn1015@naver.com>
2025-04-22 17:23:26 +0900

    remove: 공통 함수 묶기

--- src/pages/CallPage/EmergencyCallPage/index.tsx
+++ src/pages/CallPage/EmergencyCallPage/index.tsx
@@ -11,54 +11,8 @@ import Lottie from 'lottie-react';
 import videoLoading from 'src/assets/video-loading.json';
 import Video from '../components/Video';
 import EmergencyReportModal from '../components/EmergencyReportModal';
+import { formatTime, formatKoreanDate } from '../../../utils/functions/formatTime';
 
-// 시간 포맷 (예: 00:02:15)
-const formatTime = (seconds: number, type: 'digit' | 'korean') => {
-  const h = String(Math.floor(seconds / 3600)).padStart(2, '0');
-  const m = String(Math.floor((seconds % 3600) / 60)).padStart(2, '0');
-  const s = String(seconds % 60).padStart(2, '0');
-  if (type === 'digit') {
-    return `${h}:${m}:${s}`;  
-  }
-
-  if (type === 'korean') {
-    if (h === '00' && m === '00') {
-      return `${s}초`;
-    }
-    if (h === '00') {
-      return `${m}분 ${s}초`;
-    }
-    return `${h} ${m}분 ${s}초`;
-  }
-
-  return '0';
-};
-
-const formatKoreanDate = (date: Date | null, type: 'digit' | 'korean'): string => {
-  if (!date) return '';
-  const year = date.getFullYear();
-  const month = String(date.getMonth() + 1).padStart(2, '0'); // 0-based month
-  const day = String(date.getDate()).padStart(2, '0');
-
-  const hour = String(date.getHours()).padStart(2, '0');
-  const minute = String(date.getMinutes()).padStart(2, '0');
-
-  if (type === 'digit') {
-    return `${year}.${month}.${day}  ${hour}:${minute}`;
-  }
-
-  return `${year}년 ${month}월 ${day}일   ${hour}시 ${minute}분`;
-};
-
-// 응급기관의 경우
-// 1. 농인이 아직 안 들어온 경우
-// 2. 농인이 들어온 경우
-
-// 농인의 경우
-// 1. 응급기관이 접수를 아직 안 한 경우
-// 2. 응급기관이 접수를 한 경우
-
-// 농인과 응급기관만 사용하는 페이지
 export default function EmergencyCallPage() {
   const params = useParams();
   const navigate = useNavigate();
